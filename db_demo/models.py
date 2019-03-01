d2bd5284749cea9f719fe7b7d800004c43a68095
Bob Hunt <bob.hunt.email@gmail.com>
2019-03-01 14:50:05 -0500

    Populated models.py with data from sprint_a.

--- db_demo/models.py
+++ db_demo/models.py
@@ -4,4 +4,47 @@ from django.db import models
 # username, password, email address, and so on.
 from django.contrib.auth.models import User
 
-# Your models go here!
+class Users(models.Model):
+    id = INT
+    username = models.VARCHAR(45)
+    password = models.VARCHAR(45)
+    email = VARCHAR(45)
+
+class Profiles(models.Model):
+    id = Int
+    user_id = INT
+    avatar = VARCHAR(45)
+    body = TEXT
+
+class Roles(models.Model):
+    id = INT
+    title = VARCHAR(45)
+
+class Contributors(models.Model):
+    page_id = INT
+    user_id = INT
+    role_id = INT
+
+class Permissions(models.Model):
+    id = INT
+    title = VARCHAR(45)
+
+class Pages(models.Model):
+    id=INT
+    user_id = INT
+    category_id = INT
+    title = VARCHAR(124)
+    body = LONGTEXT
+    is_published = TINYINT
+    is_flagged = TINYINT
+
+class Categories(models.Model):
+    id = INT
+    user_id = INT
+    parent_id = INT
+    title = VARCHAR(30)
+
+class Tags(models.Model):
+    id = INT
+    user_id = INT
+    body = VARCHAR(50)
